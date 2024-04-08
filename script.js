b21adb3d0cda8dd755e398628b2954fca8f8aaf9
BFF <furkat@MacBook-Air-Furkat-2.local>
2024-04-08 15:05:12 +0500

    hfrufhruhfru

---
+++ script.js
@@ -0,0 +1,31 @@
+// //Составить предложение
+// let arr = [true, false, "Hello", 2, 3, 5, " My name is ", ["Looser", "Farzod"], "and I am ", 2,3, 33, 12, 16, " years old"]
+// console.log(arr[2] + " " + arr[6] + " " + arr[7][1] + " " + arr[8] + " " + arr[13] + " " + arr[14]);
+
+// Подсчитать сумму чисел
+let arr = [
+    [
+        1,
+        2,
+        3,
+        true,
+        false,
+        [
+            "s",
+            true,
+            100,
+            [
+                20
+            ],
+            "s"
+        ],
+        [
+            {a: 4},
+            {b: 10}
+        ]
+    ]
+]
+
+let total = 0
+
+console.log(arr[0][0] + " " + arr[0][1] + " " + arr[0][2] + " " + arr[0][5][2] + " " + arr[0][5][3] + " " + arr[0][5][3][]);
